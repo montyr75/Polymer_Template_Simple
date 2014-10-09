06047b5a181168a6b4843d9f76b0f5503ff86d8e
Monty Rasmussen <montyr75@gmail.com>
2014-10-09 13:03:25 -0600

    first commit

---
+++ web/src/views/app_view/app_view.dart
@@ -0,0 +1,35 @@
+library app_view;
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('app-view')
+class AppView extends PolymerElement {
+
+  static const CLASS_NAME = "AppView";
+
+  // constants
+  static const String SAMPLE_CONSTANT = "SAMPLE_CONSTANT";
+
+  @observable String bindingTest = "Binding is working...";
+
+  // non-visual initialization can be done here
+  AppView.created() : super.created();
+
+  // life-cycle method called by the Polymer framework when the element is attached to the DOM
+  @override void attached() {
+    super.attached();
+    print("$CLASS_NAME::attached()");
+  }
+
+  // a sample event handler function
+  void eventHandler(Event event, var detail, Element target) {
+    print("$CLASS_NAME::eventHandler()");
+  }
+
+  void submit(Event event, var detail, Element target) {
+    // prevent app reload on <form> submission
+    event.preventDefault();
+  }
+}
+
